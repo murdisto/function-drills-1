0f262c50edd928eef8cff67a3a9b50cd13afd4bd
Lauren Snow <laursnowie@gmail.com>
2018-10-29 14:15:42 -0400

    6th drill

--- function-drills-1.js
+++ function-drills-1.js
@@ -1,7 +1,11 @@
+function getYearofBirth(age) {
+    const yearOfBirth = 2018 - age;
+}
 function createGreeting(name, age) {
-  const yearOfBirth = 2018 - age;
-    return `Hi, my name is ${name} and I am ${age} years old. I was born in ${yearOfBirth}.`;
+    const yob = getYearofBirth(age);
+    return `Hi, my name is ${name} and I am ${age} years old. I was born in ${yob}.`;
 }
 
 const greeting1 = createGreeting();
 console.log(greeting1);
+
