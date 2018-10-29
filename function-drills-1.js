b3758ac0e9e51f975c68cfab95f3a8b6393067c2
Scott <22904147+murdisto@users.noreply.github.com>
2018-10-29 11:09:47 -0700

    Delete function-drills-1.js

--- function-drills-1.js
+++
@@ -1,7 +0,0 @@
-function createGreeting(name, age) {
-    const yearOfBirth = 2018 - age;
-    return 'Hi, my name is Lauren and Im 18';
-}
-
-const greeting1 = createGreeting();
-console.log(greeting1);
