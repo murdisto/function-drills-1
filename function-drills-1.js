725f628706020d1d39726b19945f6ebdd86aca45
Scott Ian <sim2487@gmail.com>
2018-10-29 11:27:07 -0700

    seventh drill

--- function-drills-1.js
+++ function-drills-1.js
@@ -1,11 +1,22 @@
 function getYearofBirth(age) {
+    if (age < 0){
+      throw new Error("Age can not be negative");
+    }
     const yearOfBirth = 2018 - age;
+    return yearOfBirth;
 }
 function createGreeting(name, age) {
+    if (name === undefined || age === undefined){
+      throw new Error('Arguments not valid')
+    }
+
     const yob = getYearofBirth(age);
     return `Hi, my name is ${name} and I am ${age} years old. I was born in ${yob}.`;
 }
 
-const greeting1 = createGreeting();
-console.log(greeting1);
-
+try{
+  const greeting1 = createGreeting("John", -24);
+  console.log(greeting1);
+} catch(error) {
+  console.error(error);
+}
