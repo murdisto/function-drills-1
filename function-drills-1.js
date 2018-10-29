3ac6d040f35ad2b086b638aa14389f4eb48933aa
Lauren Snow <laursnowie@gmail.com>
2018-10-29 14:36:22 -0400

    all drills complete

--- function-drills-1.js
+++ function-drills-1.js
@@ -1,21 +1,28 @@
+'use strict';
+
 function getYearofBirth(age) {
-    if (age < 0){
-      throw new Error("Age can not be negative");
-    }
-    const yearOfBirth = 2018 - age;
-    return yearOfBirth;
+  if (age < 0){
+    throw new Error('Age can not be negative');
+  }
+  const yearOfBirth = 2018 - age;
+  return yearOfBirth;
 }
 function createGreeting(name, age) {
-    if (name === undefined || age === undefined){
-      throw new Error('Arguments not valid')
-    }
-
-    const yob = getYearofBirth(age);
-    return `Hi, my name is ${name} and I am ${age} years old. I was born in ${yob}.`;
+  if (name === undefined || age === undefined){
+    throw new Error('Arguments not valid');
+  }
+  if (typeof name !== 'string') {
+    throw new TypeError('Name must be a string');
+  }
+  if (typeof age !== 'number') {
+    throw new TypeError('Age must be a number');  
+  }
+  const yob = getYearofBirth(age);
+  return `Hi, my name is ${name} and I am ${age} years old. I was born in ${yob}.`;
 }
 
 try{
-  const greeting1 = createGreeting("John", -24);
+  const greeting1 = createGreeting('John', 24);
   console.log(greeting1);
 } catch(error) {
   console.error(error);
